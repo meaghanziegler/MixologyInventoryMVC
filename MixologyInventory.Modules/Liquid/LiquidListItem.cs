c7f73482e478cd9206ff71f8d3ad50faa041ff79
Meaghan Ziegler <meaghan.ziegler@gmail.com>
2022-01-13 14:59:01 -0500

    Liquid Controller and Index View Made

---
+++ MixologyInventory.Modules/Liquid/LiquidListItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MixologyInventory.Modules.Liquid
+{
+    public class LiquidListItem
+    {
+        public int LiquidID { get; set; }
+        public string Brand { get; set; }
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
---
+++ MixologyInventory.WebMVC/Controllers/LiquidController.cs
@@ -0,0 +1,20 @@
+﻿using MixologyInventory.Modules.Liquid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MixologyInventory.WebMVC.Controllers
+{
+    [Authorize]
+    public class LiquidController : Controller
+    {
+        // GET: Liquid
+        public ActionResult Index()
+        {
+            var model = new LiquidListItem[0];
+            return View(model);
+        }
+    }
+}
