3f2c0ff5804852383e4d0b8fb08edf39c1a0b547
Meaghan Ziegler <meaghan.ziegler@gmail.com>
2022-01-18 10:44:28 -0500

    Drink Index and DrinkListItem Added

---
+++ MixologyInventory.Modules/Drink/DrinkListItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MixologyInventory.Model.Drink
+{
+    public class DrinkListItem
+    {
+        public int RecipeID { get; set; }
+        public string DrinkName { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ MixologyInventory.WebMVC/Controllers/DrinkController.cs
@@ -0,0 +1,20 @@
+﻿using MixologyInventory.Model.Drink;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MixologyInventory.WebMVC.Controllers
+{
+    [Authorize]
+    public class DrinkController : Controller
+    {
+        // GET: Drink
+        public ActionResult Index()
+        {
+            var model = new DrinkListItem[0];
+            return View(model);
+        }
+    }
+}
