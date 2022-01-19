c56a97e171e52012ed61013b52a35484e2a72c99
Meaghan Ziegler <meaghan.ziegler@gmail.com>
2022-01-19 11:06:03 -0500

    Drink Create and Recipe Tab Added

---
+++ MixologyInventory.Modules/Drink/DrinkCreate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MixologyInventory.Model.Drink
+{
+    public class DrinkCreate
+    {
+        [Required]
+        public string DrinkName { get; set; }
+        public string LiquidName { get; set; }
+        public decimal LiquidAmount { get; set; }
+        public string Directions { get; set; }
+        public string Description { get; set; }
+    }
+}
--- MixologyInventory.WebMVC/Controllers/DrinkController.cs
+++ MixologyInventory.WebMVC/Controllers/DrinkController.cs
@@ -16,5 +16,22 @@ namespace MixologyInventory.WebMVC.Controllers
             var model = new DrinkListItem[0];
             return View(model);
         }
+
+        // GET
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(DrinkCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(model);
+        }
     }
 }
